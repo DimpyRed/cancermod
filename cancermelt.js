99159dd6d538d603046718eee071e38d11e72738
= <=>
2022-11-16 21:43:35 -0800

    added liquid cancer

--- cancermelt.js
+++ cancermelt.js
@@ -1,1 +1,51 @@
-elements.cancer.stateHigh = "infection";
+elements.liquid_cancer = {
+    color: ["#300b29","#5c114e","#870c71"],
+    behavior: behaviors.LIQUID,
+    reactions: {
+        "cell": { "elem2":"cancer", "chance":0.005 },
+        "frog": { "elem2":"cancer", "chance":0.005 },
+        "tadpole": { "elem2":"cancer", "chance":0.005 },
+        "fish": { "elem2":"cancer", "chance":0.005 },
+        "rat": { "elem2":"cancer", "chance":0.005 },
+        "bird": { "elem2":"cancer", "chance":0.005 },
+        "sugar": { "elem2":"liquid_cancer", "chance":0.04 },
+        "sugar_water": { "elem2":"liquid_cancer", "chance":0.05 },
+        "": { "elem1":[null,"dna"], "chance":0.01 },
+        "poison": { "elem1":null, "chance":0.01 },
+        "proton": { "elem1":null, "chance":0.04 },
+    },
+    tempHigh: 115,
+    stateHigh: "plague",
+    state: "liquid",
+    density: 1035,
+    category: "life",
+    breakInto: ["dirty_water","dna","dna","dna","dna"]
+};
+
+
+elements.pool_water.reactions.liquid_cancer = elements.pool_water.reactions.cancer;
+elements.meat.reactions.liquid_cancer = elements.meat.reactions.cancer;
+elements.body.reactions.liquid_cancer = elements.body.reactions.cancer;
+elements.null.reactions.liquid_cancer = elements.null.reactions.cancer;
+elements.head.reactions.liquid_cancer = elements.head.reactions.cancer;
+elements.bleach.reactions.liquid_cancer = elements.bleach.reactions.cancer;
+elements.blood.reactions.liquid_cancer = elements.blood.reactions.cancer;
+elements.vaccine.reactions.liquid_cancer = elements.vaccine.reactions.cancer;
+elements.antibody.reactions.liquid_cancer = elements.antibody.reactions.cancer;
+elements.primordial_soup.reactions.liquid_cancer = elements.primordial_soup.reactions.cancer;
+
+
+elements.radiation.reactions.blood = { "elem2":"liquid_cancer", "chance":0.4 };
+
+elements.cancer.stateHigh = "liquid_cancer";
+elements.cancer.reactions.liquid_cancer = {"elem2": "cancer", "chance":0.005};
+elements.cancer.breakInto = ["liquid_cancer", "liquid_cancer", "liquid_cancer", "dna"];
+
+elements.cancer.reactions.poison = { "elem1": "liquid_cancer", "chance": 0.04 };
+elements.bleach.reactions.cancer = { "elem2": "liquid_cancer", "chance": 0.04 };
+elements.vaccine.reactions.cancer = { "elem2":"liquid_cancer", "chance":0.04 };
+elements.antibody.reactions.cancer = { "elem2":"liquid_cancer", "chance":0.04 };
+
+
+
+elements.virus.ignore = ["fire","smoke","soap","plague","cancer", "liquid_cancer"];
